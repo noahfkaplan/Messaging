3171f101bba8ed8e220e69c6a41f2d1598ff89e3
Noah Kaplan <nkaplan1@uncc.edu>
2018-09-09 18:58:40 -0400

    Beginning implementing server session

---
+++ Messaging/Messaging/IServerSessionActions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Messaging
+{
+    interface IServerSessionActions
+    {
+        int Bind();
+        int Listen();
+        int AcceptConnection();
+        int ReceiveMessage();
+        int SendMessage();
+
+    }
+}
---
+++ Messaging/Messaging/SocketsServerSessionActions.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Messaging
+{
+    public class SocketsServerSessionActions : IServerSessionActions
+    {
+        private List<IClientConnection> _SessionMembers;
+        private Socket _socket;
+
+        public SocketsServerSessionActions()
+        {
+            _socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        }
+
+        public int AcceptConnection()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Bind()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Listen()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int ReceiveMessage()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int SendMessage()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
